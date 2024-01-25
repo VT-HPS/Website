8a6feb8106da2535e085ca97d1d0ce06926ed0da
runnerjoe28 <osuchjm@gmail.com>
2024-01-25 16:24:38 -0500

    Remove body tags from Home and Membership jsx

--- src/Pages/Home.jsx
+++ src/Pages/Home.jsx
@@ -6,34 +6,32 @@ import RotatingImages from "../Components/RotatingImages";
  */
 const Home = () => {
     return (
-        <body>
-            <div className="main_page_container">
-                <div>
-                    <header className="home-header">
-                        <h1 className="centered_text">What is Human Powered Submarine at Virginia Tech?</h1>
-                        <p className="centered_text">Interdisciplinary Student Design Team</p>
-                    </header>
-                </div>
-                <div className="rotating_images">
-                    <RotatingImages />
-                </div>
-                <div className="home_text_container">
-                    <section>
-                        <h2 className="centered_text">About The Team</h2>
-                        <p className="centered_text">
-                            Hello! We are HPS at VT and we design, build, and race submarines
-                        </p>
-                    </section>
-
-                    <section>
-                        <h2 className="centered_text">Contact</h2>
-                        <p className="centered_text">
-                            Email: hps@vt.edu<br />
-                        </p>
-                    </section>
-                </div>
+        <div className="main_page_container">
+            <div>
+                <header className="home-header">
+                    <h1 className="centered_text">What is Human Powered Submarine at Virginia Tech?</h1>
+                    <p className="centered_text">Interdisciplinary Student Design Team</p>
+                </header>
             </div>
-        </body>
+            <div className="rotating_images">
+                <RotatingImages />
+            </div>
+            <div className="home_text_container">
+                <section>
+                    <h2 className="centered_text">About The Team</h2>
+                    <p className="centered_text">
+                        Hello! We are HPS at VT and we design, build, and race submarines
+                    </p>
+                </section>
+
+                <section>
+                    <h2 className="centered_text">Contact</h2>
+                    <p className="centered_text">
+                        Email: hps@vt.edu<br />
+                    </p>
+                </section>
+            </div>
+        </div>
 
     );
 }
--- src/Pages/Membership.jsx
+++ src/Pages/Membership.jsx
@@ -8,21 +8,19 @@ import Stack from 'react-bootstrap/Stack';
  */
 const Membership = () => {
     return (
-        <body>
-            <div className="membership_main_container">
-                <div className="membership_left">
-                <Stack gap={2} >
-                    <h1 className= "centered_text">Interested in joining HPS at VT?</h1>
-                    <h3 className= "centered_text">HPS at VT accepts students of all techincal disciplines and grade levels. 
-                    If you are interested in joining the team, please fill out the contact info form. 
-                    Alternatively, come find us in basement of the Ware Lab to learn more!</h3>
-                </Stack>
-                </div>
-                <div className="membership_right">
-                    <ContactCard />
-                </div>
+        <div className="membership_main_container">
+            <div className="membership_left">
+            <Stack gap={2} >
+                <h1 className= "centered_text">Interested in joining HPS at VT?</h1>
+                <h3 className= "centered_text">HPS at VT accepts students of all techincal disciplines and grade levels. 
+                If you are interested in joining the team, please fill out the contact info form. 
+                Alternatively, come find us in basement of the Ware Lab to learn more!</h3>
+            </Stack>
             </div>
-        </body>
+            <div className="membership_right">
+                <ContactCard />
+            </div>
+        </div>
         
     );
 }
