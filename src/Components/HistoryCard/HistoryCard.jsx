69546aac8611c684a8444057ea9baffef270d3b0
runnerjoe28 <osuchjm@gmail.com>
2024-01-26 16:50:35 -0500

    Migrate HistoryCard formatting to local css

--- src/Components/HistoryCard/HistoryCard.jsx
+++ src/Components/HistoryCard/HistoryCard.jsx
@@ -1,4 +1,5 @@
 import React from "react";
+import "./HistoryCard.css";
 
 /**
  * Creates a history card which is displayed on the history page. The card contains 
@@ -8,22 +9,22 @@ import React from "react";
 
 function History(props) {
     return (
-        <div className="history_container">
-            <div classname="history_left_column">
-                <img className="history_image" src={props.image} alt="History" id="history_image"/>
+        <div className="historycard_container">
+            <div classname="historycard_left_column">
+                <img className="historycard_image" src={props.image} alt="History" id="history_image"/>
             </div>
             <div>
-                <h1 className="history_stat" style={{fontSize: '30px'}}>{props.title}</h1>
-                <p className="history_stat"><b>Operating Years: </b>{props.operating_years}</p>
-                <p className="history_stat"><b>Length: </b>{props.length}</p>
-                <p className="history_stat"><b>Beam: </b>{props.beam}</p>
-                <p className="history_stat"><b>Crew Size: </b>{props.crew_size}</p>
-                <p className="history_stat"><b>Propulsion Class: </b>{props.propulsion_class}</p>
-                <p className="history_stat"><b>Entered In: </b>{props.entered_in}</p>
-                <p className="history_stat"><b>Awards: </b>{props.awards}</p>
-                <p className="history_stat"><b>Top Speed: </b>{props.top_speed}</p>
-                <p className="history_stat"><b>Faculty Advisor: </b>{props.faculty_advisor}</p>
-                <p className="history_stat"><b>Description: </b>{props.description}</p>
+                <h1 className="historycard_stat" style={{fontSize: '30px'}}>{props.title}</h1>
+                <p className="historycard_stat"><b>Operating Years: </b>{props.operating_years}</p>
+                <p className="historycard_stat"><b>Length: </b>{props.length}</p>
+                <p className="historycard_stat"><b>Beam: </b>{props.beam}</p>
+                <p className="historycard_stat"><b>Crew Size: </b>{props.crew_size}</p>
+                <p className="historycard_stat"><b>Propulsion Class: </b>{props.propulsion_class}</p>
+                <p className="historycard_stat"><b>Entered In: </b>{props.entered_in}</p>
+                <p className="historycard_stat"><b>Awards: </b>{props.awards}</p>
+                <p className="historycard_stat"><b>Top Speed: </b>{props.top_speed}</p>
+                <p className="historycard_stat"><b>Faculty Advisor: </b>{props.faculty_advisor}</p>
+                <p className="historycard_stat"><b>Description: </b>{props.description}</p>
             </div>
         </div>
     )
