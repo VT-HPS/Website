2ce2055746e02f542905d2e75499a3c19103f948
runnerjoe28 <osuchjm@gmail.com>
2024-01-26 16:35:36 -0500

    Migrate CopyrightTag formatting to local css

--- src/Components/CopyrightTag/CopyrightTag.jsx
+++ src/Components/CopyrightTag/CopyrightTag.jsx
@@ -1,14 +1,15 @@
 import React from "react";
+import "./CopyrightTag.css";
 /**
  * Creates a copyright tag that is always displayed at the foot
  * of the page.
  */
 function DisplayCopyrightTag() {
     return (
-        <footer className="footer">
-                <div class="text-left px-5">
-                    <small>Copyright &copy; 2024 HPS at Virginia Tech | hps@vt.edu</small>
-                </div>
+        <footer className="copyrighttag_footer">
+            <div class="text-left px-5">
+                <small>Copyright &copy; 2024 HPS at Virginia Tech | hps@vt.edu</small>
+            </div>
         </footer>
     )
 }
