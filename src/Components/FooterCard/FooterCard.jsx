fd1ffec0fde3ce4620bf52bb977669ccbd41d582
Luke Hylton <lnhylton@vt.edu>
2024-02-01 14:53:28 -0500

    Updated footer with better social icons and mobile friendly design

--- src/Components/FooterCard/FooterCard.jsx
+++ src/Components/FooterCard/FooterCard.jsx
@@ -1,7 +1,8 @@
 import React from "react";
 import "./FooterCard.css";
 import logo from '../../Assets/hps_logo1.png';
-import { BsGithub, BsInstagram } from 'react-icons/bs';
+import { SocialIcon } from 'react-social-icons'
+
 
 /* This page creates a footer, examples can be found at this website https://www.flowbite-react.com/docs/components/footer, these examples
  * were followed but not implemented with flowbite since tailwing was not as customizable as css
@@ -30,11 +31,14 @@ const PageFooter = () => {
                         &copy; <a className="text-white" href="/">2024 HPS at Virginia Tech</a> | hps@vt.edu
                     </p>
                     <div className="social-icon-container">
-                        <a className="social-icon" href="https://www.instagram.com/vthps/">
-                            <BsInstagram className="text-white" />
+                        <a className="social-icon">
+                            <SocialIcon network="instagram" url="https://www.instagram.com/vthps/"/>
                         </a>
-                        <a className="social-icon" href="https://github.com/VT-HPS">
-                            <BsGithub className="text-white" />
+                        <a className="social-icon">
+                            <SocialIcon network="linkedin" url="https://www.linkedin.com/company/human-powered-submarine-at-virginia-tech/about/" />
+                        </a>
+                        <a className="social-icon">
+                            <SocialIcon network="github" url="https://github.com/VT-HPS" />
                         </a>
                     </div>
                 </div>
