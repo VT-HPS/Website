4ddd52ded5e86ce8aa6b08fd2f3581aba14be034
WyattDun <wyattd@vt.edu>
2024-09-01 20:01:45 -0400

    Latest update for Luke

--- src/Pages/Membership.jsx
+++ src/Pages/Membership.jsx
@@ -12,17 +12,17 @@ const Membership = () => {
             <div className="membership_left">
             <Stack gap={2} >
                 <h1 className= "membership_centered_text">Interested in joining HPS at VT?</h1>
-                <h3 className= "membership_centered_text">HPS at VT accepts students of all techincal disciplines and grade levels. 
+                <h3 className= "membership_centered_text">HPS at VT accepts students of all technical disciplines and grade levels. 
                 If you are interested in joining the team, please fill out the contact info form. 
                 Alternatively, come find us in basement of the Ware Lab to learn more!</h3>
                 <br/>
-                <h3 className="membership_centered_text">The application deadline has passed for this semester. 
-                Thank you for showing interest in our team. The link to applications will be posted here early next semester.</h3>
-                {/*<h3 className="membership_centered_text">Thank you for showing interest in our team. Please click 
-                <a href="https://forms.office.com/r/Lax9BipSMj" target="_blank"> here </a> to access the application. The application closes on 
-                Friday, January 26, 2024 at 11:59pm. If you have any questions regarding joining us, please contact us at hps@vt.edu.</h3>
-                <h3 className="membership_centered_text"> If the link above does not work here is the full link 
-                <a href="https://forms.office.com/r/Lax9BipSMj" target="_blank">https://forms.office.com/r/Lax9BipSMj</a></h3>*/}
+                {/*<h3 className="membership_centered_text">The application deadline has passed for this semester. 
+                Thank you for showing interest in our team. The link to applications will be posted here early next semester.</h3>*/}
+                <h3 className="membership_centered_text">Thank you for showing interest in our team. Please click 
+                <a href="https://forms.office.com/r/G7C4ScQdD3" target="_blank"> here </a> to access the application. The application closes on 
+                Friday, September 13, 2024 at 11:59pm. If you have any questions regarding joining us, please contact us at hps@vt.edu.</h3>
+                <h3 className="membership_centered_text"> If the link above does not work here is the full link  
+                <a href="https://forms.office.com/r/G7C4ScQdD3" target="_blank"> https://forms.office.com/r/G7C4ScQdD3</a></h3>
             </Stack>
             </div>
             <div className="membership_right">
