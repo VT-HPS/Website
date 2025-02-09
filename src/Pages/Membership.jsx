0105d218681e4db01e703f6eb6d8709bbd8943d7
WyattDun <wyattd@vt.edu>
2025-02-09 14:05:45 -0500

    Update to the membership page

--- src/Pages/Membership.jsx
+++ src/Pages/Membership.jsx
@@ -16,11 +16,13 @@ const Membership = () => {
                 If you are interested in joining the team, please fill out the contact info form. 
                 Alternatively, come find us in basement of the Ware Lab to learn more!</h3>
                 <br/>
-                <h3 className="membership_centered_text">Thank you for showing interest in our team. Please click 
+                <h3 className="membership_centered_text">The application deadline has passed for this semester. 
+                Thank you for showing interest in our team. The link to applications will be posted here early next semester.</h3>
+                {/*<h3 className="membership_centered_text">Thank you for showing interest in our team. Please click 
                 <a href="https://forms.office.com/r/Cya6URTHy1" target="_blank"> here </a> to access the application. The application closes on 
                 Friday, January 31, 2025 at 11:59pm. If you have any questions regarding joining us, please contact us at hps@vt.edu.</h3>
                 <h3 className="membership_centered_text"> If the link above does not work here is the full link  
-                <a href="https://forms.office.com/r/Cya6URTHy1" target="_blank"> https://forms.office.com/r/Cya6URTHy1</a></h3>
+                <a href="https://forms.office.com/r/Cya6URTHy1" target="_blank"> https://forms.office.com/r/Cya6URTHy1</a></h3>*/}
             </Stack>
             </div>
             <div className="membership_right">
